dad5c336832d27b44d61296d3fe6cb7f8423fbc8
Robert Cabrera <rscabrera@gmail.com>
2016-09-10 20:05:50 +0000

    /added express and some routing

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,15 @@
-var name = 'My name is Robert Cabrera';
-console.log(name);
+var express = require("express");
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello World!');
+});
+
+app.get('/routing', function(req, res){
+    res.send('Hello Routing!');
+});
+
+var port = process.env.PORT;
+app.listen(port, function(err){
+    console.log('The server is running on port: ' + port);
+});
